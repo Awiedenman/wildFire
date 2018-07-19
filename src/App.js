7aaa93dba8b4d38dc8096ba6f6e9850906f2244d
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-19 12:53:29 -0600

    Make initial fetch request for current fires and convert xml to json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+var convert = require('xml-js');
+
 
 class App extends Component {
+
+  componentDidMount(){
+    this.currentFireRequest();
+
+  }
+
+  currentFireRequest = async () => {
+    const url = 'https://cors-anywhere.herokuapp.com/https://www.geomac.gov/DynContent/georss/nifc_large_firesW3C.xml';
+    const xmlResponse = await fetch(url);
+    // consorle.log(xmlData)
+    const currentFireData = await xmlResponse.text();
+    var result = convert.xml2json(
+      currentFireData, {
+        compact: true,
+        spaces: 2
+      });
+    console.log(result)
+    console.log(currentFireData)
+    return currentFireData
+
+  }
+
   render() {
     return (
-      <div className="App">
+      <main>
         <header>
         </header>
-      
-      </div>
+      </main>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Provider, createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
 
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore({
-  rootReducer, reduxDevTools
-});
+// const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const store = createStore({
+//   rootReducer, reduxDevTools
+// });
 
 ReactDOM.render(
-<Provider store={store}>
+<Provider>
   <App />
 </Provider>, document.getElementById('root'));
-registerServiceWorker();
