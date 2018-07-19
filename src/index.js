8ec42b72dd39cdb8a20b4f9c0d9df5e5fddc2633
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-19 12:33:28 -0600

    Add Provider to index.js and give context with store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider, createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore({
+  rootReducer, reduxDevTools
+});
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
