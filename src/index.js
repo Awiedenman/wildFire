82f5e1ec8c46d419e7fa02bfaf9ec665b851b297
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-19 15:22:33 -0600

    Create rootReducer

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { rootReducer } from './reducers';
 
 const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore({
-  rootReducer, reduxDevTools
-});
+const store = createStore(rootReducer, reduxDevTools);
 
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
   <App />
 </Provider>, document.getElementById('root'));
--- src/reducers/currentFireReducer.js
+++ src/reducers/currentFireReducer.js
@@ -3,9 +3,9 @@ import { storeCurrentFireData } from '../actions';
 export const currentFireReducer = (state=[], action) => {
   switch (action.type) {
     case 'STORE_CURRENT_FIRE_DATA':
-    return [...store, action.currentFireData]
+    return [...state, action.currentFireData]
   
     default:
-      return store;
+      return state;
   }
 }
