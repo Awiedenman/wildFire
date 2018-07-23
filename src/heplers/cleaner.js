775bc927796fd216c054bc44e6becae9e369c113
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-23 13:00:30 -0600

    Clean fire images

--- src/heplers/cleaner.js
+++ src/heplers/cleaner.js
@@ -1,7 +1,9 @@
 export const fireDataCleaner = (fireData) => {
   const cleanFireData = fireData.rss.channel.item.reduce((fireObject, fire )=> {
+    // console.log(fire)
     // console.log(fire.title._text.split(',')[0]);
-    fireObject['name'] = fire.title._text.split(',')[0]
+    fireObject['name'] = fire.title._text.split(',')[0],
+    fireObject['image'] = fire.enclosure._attributes.url,
     console.log(fireObject)
     return fireObject
   }, {})
