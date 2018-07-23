2fa8b4c3f5d15e2fe713fb4529989be225adcad7
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-23 13:16:44 -0600

    Clean fre latitude and longitude

--- src/heplers/cleaner.js
+++ src/heplers/cleaner.js
@@ -1,10 +1,14 @@
 export const fireDataCleaner = (fireData) => {
   const cleanFireData = fireData.rss.channel.item.reduce((fireObject, fire )=> {
-    // console.log(fire)
+    console.log(fire)
     // console.log(fire.title._text.split(',')[0]);
     fireObject['name'] = fire.title._text.split(',')[0],
-    fireObject['image'] = fire.enclosure._attributes.url
-    fireObject['acresBurned'] = fire.description._text.split(',')[1]
+    fireObject['image'] = fire.enclosure._attributes.url,
+    fireObject['acresBurned'] = fire.description._text.split(',')[1],
+    fireObject['lastUpdate'] = fire.description._text.split(',')[0]
+    fireObject['latitude'] = fire['geo:lat']._text,
+    fireObject['longitude'] = fire['geo:long']._text
+
 
     console.log(fireObject)
     return fireObject
