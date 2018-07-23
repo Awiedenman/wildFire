d855ba95fe3bf1a56ce6340c25ed8195dd3f2ae1
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-23 13:02:54 -0600

    Clean fire acresBurned

--- src/heplers/cleaner.js
+++ src/heplers/cleaner.js
@@ -3,7 +3,9 @@ export const fireDataCleaner = (fireData) => {
     // console.log(fire)
     // console.log(fire.title._text.split(',')[0]);
     fireObject['name'] = fire.title._text.split(',')[0],
-    fireObject['image'] = fire.enclosure._attributes.url,
+    fireObject['image'] = fire.enclosure._attributes.url
+    fireObject['acresBurned'] = fire.description._text.split(',')[1]
+
     console.log(fireObject)
     return fireObject
   }, {})
