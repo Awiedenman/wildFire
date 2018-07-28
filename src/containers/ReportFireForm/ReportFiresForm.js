4ab1bb92a8231ac48025744fa6e38c90e7b268e5
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-28 16:44:33 -0600

    Build out form component

---
+++ src/containers/ReportFireForm/ReportFiresForm.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+
+export class ReportFiresForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      latitude: '',
+      longitude: '',
+      city: '',
+      state:'',
+      zipCode: ''
+    }
+  }
+  handleChange = (e) =>{
+    const { value, name } =e.target;
+    this.setState({[name]: value})
+  }
+
+  render() {
+    return (
+      <form>
+        <section className='reportFire-form-section'>
+        <h3>Name</h3>
+          <label for="name"></label>
+            <input 
+              onChange={this.handleChange} 
+              name='firstName' 
+              value={this.state.firstName}
+              id='name'
+              placeholder='First Name'
+            />
+          <label for="lastName"></label>
+            <input 
+              onChange={this.handleChange} 
+              name='lastName' 
+              value={this.state.lastName}
+              id='lastName'
+              placeholder='Last Name'
+            />
+        </section>
+        <br />
+        <section className='reportFire-form-section'>
+          <h3>(Optional)</h3>
+          <label for="latitude"></label>
+            <input 
+              onChange={this.handleChange} 
+              name='latitude' 
+              value={this.state.latitude}
+              id='latitude'
+              placeholder='Latitude'
+            />
+          <label for="longitude"></label>
+            <input 
+              onChange={this.handleChange} 
+              name='longitude' 
+              value={this.state.longitude}
+              id='longitude'
+              placeholder='Longitude'
+            />
+        </section>
+        <br />
+        <section className='reportFire-form-section'>
+          <label for="city"></label>
+            <h3>Location</h3>
+            <input 
+              onChange={this.handleChange} 
+              name='city' 
+              value={this.state.city}
+              id='city'
+              placeholder='City'
+            />
+          <label for="state"></label>
+            <input 
+              onChange={this.handleChange} 
+              name='state' 
+              value={this.state.state}
+              id='state'
+              placeholder='State'
+            />
+          <label for="zip code"></label>
+            <input 
+              onChange={this.handleChange} 
+              name='zipCode' 
+              value={this.state.zipCode}
+              id='zipCode'
+              placeholder='Zip Code'
+            />  
+        </section>
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
--- src/containers/ReportFires/ReportFires.js
+++ src/containers/ReportFires/ReportFires.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react'
 import CurrentFires from '../CurrentFires/CurrentFires';
+import { ReportFiresForm } from '../ReportFireForm/ReportFiresForm';
 
 import './ReportFires.css';
 
 export class ReportFires extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      location: ''
-    }
-  }
-
-  handleChange = (e) =>{
-    const { value } =e.target;
-    this.setState({location: value})
-  }
-
+  
   render(){
     return(
-      <div>
+      <div className='reportFires'>
         <div className='reportFires-form'>
-          <form>
-            <input 
-              onChange={this.handleChange} 
-              name='location' 
-              value={this.state.location}
-            />
-          </form>
+          <ReportFiresForm />
         </div>
         <div className='reportFires-map'>
           <CurrentFires/>
