152c256b394f29933e15b42ffcfa7a4714580218
Austin.Wiedenman <awiedenman@gmail.com>
2018-07-31 12:10:16 -0600

    Add and run eslint

--- src/containers/ReportFiresForm/ReportFiresForm.js
+++ src/containers/ReportFiresForm/ReportFiresForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 // import { postUnverifiedFires } from '../../heplers/apiCalls/apiCalls';
 
 export class ReportFiresForm extends Component {
